cc2af05554fa7e307b3a13f62fbe498a14b1a851
Smahle <stianmahle@gmail.com>
2024-11-29 18:38:49 +0100

    rst

--- src/components/TaskManager.tsx
+++ src/components/TaskManager.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import TaskManagerList from "./TaskManagerList";
 
 export default function TaskManager() {
-  return <TaskManagerList />;
-  //todo: background component
-  //todo: list component, add card button, be able to drag and drop cards between lists / color for different users maybe
-  //todo: card component, with detail description, deadline, vedlegg, and comments
+  return (
+    <div style={{ backgroundColor: "green", minHeight: "100vh" }}>
+      <TaskManagerList />
+    </div>
+  );
 }
+
